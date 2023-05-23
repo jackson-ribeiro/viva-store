1f9c0c53950395272dd24e0f1940995e7e9be0d3
Gabriel Souza <gabrielsouzase@gmail.com>
2023-05-22 21:27:13 -0300

    Update home_page.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,20 +1,50 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  final GlobalKey<ScaffoldState> _key = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(children: [
-        //bom dia
+    return Scaffold(
+      key: _key,
+      drawer: Drawer(),
+      appBar: AppBar(
+        title: Text(
+          "Viva Store",
+          style: TextStyle(color: Colors.black),
+        ),
+        centerTitle: true,
+        elevation: 0.0,
+        backgroundColor: Colors.grey[100],
+        leading: IconButton(
+          icon: Icon(
+            Icons.menu,
+            color: Colors.black,
+          ),
+          onPressed: () {
+            _key.currentState?.openDrawer();
+          },
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.notifications_none, color: Colors.black),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.send, color: Colors.black),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        child: Column(
+          children: <Widget>[
 
-        //vamos ordernar os produtos
-
-        //divider
-
-        //itens frescos + grid
-      ]),
+          ],
+        ),
+      ),
     );
   }
 }
