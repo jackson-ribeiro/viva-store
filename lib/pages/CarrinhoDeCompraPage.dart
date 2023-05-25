24fec59f27d974a9b90bff5debbde8ef3f9120a5
Gabriel Souza <gabrielsouzase@gmail.com>
2023-05-25 18:30:53 -0300

    Create CarrinhoDeCompraPage.dart

---
+++ lib/pages/CarrinhoDeCompraPage.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import '../Class/Produto.dart';
+
+class CarrinhoDeCompraPage extends StatelessWidget {
+  final List<Produto> carrinho;
+
+  const CarrinhoDeCompraPage({Key? key, required this.carrinho})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Carrinho de Compras'),
+      ),
+      body: ListView.builder(
+        itemCount: carrinho.length,
+        itemBuilder: (context, index) {
+          final produto = carrinho[index];
+          return ListTile(
+            leading: CachedNetworkImage(
+              imageUrl: produto.imageUrl,
+              placeholder: (context, url) => CircularProgressIndicator(),
+              errorWidget: (context, url, error) => Icon(Icons.error),
+              height: 50,
+              width: 50,
+            ),
+            title: Text(produto.nome_prod),
+            subtitle: Text(produto.desc_prod),
+          );
+        },
+      ),
+    );
+  }
+}
