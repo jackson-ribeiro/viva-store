4cfaf734237be7845548603971734f9992b6456e
Gabriel Souza <gabrielsouzase@gmail.com>
2023-05-27 12:53:02 -0300

    Update product_detail_page.dart

--- lib/pages/product_detail_page.dart
+++ lib/pages/product_detail_page.dart
@@ -27,13 +27,12 @@ class _ProductPageState extends State<ProductPage> {
         child: Stack(
           children: [
             Positioned.fill(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Expanded(
-                    child: Container(
-                      alignment: Alignment.topCenter,
-                      height: MediaQuery.of(context).size.height / 1.7,
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: MediaQuery.of(context).size.width,
                       color: const Color.fromARGB(255, 224, 224, 224),
                       child: CachedNetworkImage(
                         imageUrl: widget.produto.imageUrl,
@@ -43,90 +42,95 @@ class _ProductPageState extends State<ProductPage> {
                         errorWidget: (context, url, error) => Icon(Icons.error),
                       ),
                     ),
-                  ),
-                  SizedBox(height: 10),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 15),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          widget.produto.nome_prod,
-                          style: TextStyle(
-                            fontSize: 25,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(height: 2),
-                        Text(
-                          'R\$ ${widget.produto.valor.toStringAsFixed(2)}',
-                          style: TextStyle(
-                            fontSize: 25,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(height: 2),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              widget.produto.categoria,
-                              style: TextStyle(
-                                fontSize: 18,
-                                color: Color.fromARGB(255, 104, 104, 104),
-                              ),
+                    SizedBox(height: 10),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 15),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            widget.produto.nome_prod,
+                            style: TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
                             ),
-                            Text(
-                              widget.produto.estoque > 0
-                                  ? 'Estoque: ${widget.produto.estoque}'
-                                  : 'Indisponível',
-                              style: TextStyle(
-                                color: widget.produto.estoque > 0
-                                    ? Color.fromARGB(255, 15, 181, 32)
-                                    : Colors.red,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 16.0,
-                              ),
-                            ),
-
-                          ],
-                        ),
-                        SizedBox(height: 20),
-                        Text(
-                          widget.produto.desc_prod,
-                          style: TextStyle(
-                            color: Color.fromARGB(255, 103, 103, 103),
                           ),
-                        ),
-                        SizedBox(height: 30),
-                        Row(
-                          children: [
-                            Expanded(
-                              child: ElevatedButton(
-                                onPressed: () {
-                                  _adicionarAoCarrinho();
-                                },
-                                child: Padding(
-                                  padding: EdgeInsets.all(15.0),
-                                  child: Text(
-                                    'Adicionar ao carrinho',
-                                    style: TextStyle(
-                                      fontSize: 18.0,
-                                      fontWeight: FontWeight.w900,
+                          SizedBox(height: 2),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                widget.produto.categoria,
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  color: Color.fromARGB(255, 104, 104, 104),
+                                ),
+                              ),
+                              Text(
+                                'R\$ ${widget.produto.valor.toStringAsFixed(2)}',
+                                style: TextStyle(
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 2),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              Text(
+                                widget.produto.estoque > 0
+                                    ? 'Estoque: ${widget.produto.estoque}'
+                                    : 'Indisponível',
+                                style: TextStyle(
+                                  color: widget.produto.estoque > 0
+                                      ? Color.fromARGB(255, 15, 181, 32)
+                                      : Colors.red,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16.0,
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 20),
+                          Text(
+                            widget.produto.desc_prod,
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 103, 103, 103),
+                            ),
+                          ),
+                          SizedBox(height: 30),
+                          Row(
+                            children: [
+                              Expanded(
+                                child: ElevatedButton(
+                                  onPressed: widget.produto.estoque > 0
+                                      ? () => _adicionarAoCarrinho()
+                                      : null,
+                                  child: Padding(
+                                    padding: EdgeInsets.all(15.0),
+                                    child: Text(
+                                      'Adicionar ao carrinho',
+                                      style: TextStyle(
+                                        fontSize: 18.0,
+                                        fontWeight: FontWeight.w900,
+                                      ),
                                     ),
                                   ),
-                                ),
-                                style: ElevatedButton.styleFrom(
-                                  primary: Color.fromARGB(255, 48, 191, 62),
+                                  style: ElevatedButton.styleFrom(
+                                    primary: Color.fromARGB(255, 48, 191, 62),
+                                  ),
                                 ),
                               ),
-                            ),
-                          ],
-                        ),
-                      ],
+                            ],
+                          ),
+                          SizedBox(height: 20),
+                        ],
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
             Positioned(
@@ -149,5 +153,79 @@ class _ProductPageState extends State<ProductPage> {
     setState(() {
       widget.carrinho.add(widget.produto);
     });
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Dialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                CachedNetworkImage(
+                  imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnSmQAFKj7G6Nlz8i1wzZ265rArMqQROES6MO-k-riKAtBTqRb',
+                  fit: BoxFit.cover,
+                  placeholder: (context, url) => const CircularProgressIndicator(),
+                  errorWidget: (context, url, error) => const Icon(Icons.error),
+                ),
+
+                // Text(
+                //   widget.produto.nome_prod,
+                //   style: const TextStyle(
+                //     fontSize: 20.0,
+                //     fontWeight: FontWeight.bold,
+                //   ),
+                // ),
+                const SizedBox(height: 20.0),
+                RichText(
+                  textAlign: TextAlign.center,
+                  text: TextSpan(
+                    style: TextStyle(fontSize: 16.0, color: Colors.black),
+                    children: [
+                      TextSpan(
+                        text: 'O produto ',
+                      ),
+                      TextSpan(
+                        text: widget.produto.nome_prod,
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      TextSpan(
+                        text: ' foi adicionado ao carrinho.',
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 20.0),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(vertical: 10.0),
+                    child: Text(
+                      'OK',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    primary: Color.fromARGB(255, 48, 191, 62),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
   }
 }
