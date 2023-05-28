0077733adf44d0895a02ed853f77251c73f90ad9
Gabriel Souza <gabrielsouzase@gmail.com>
2023-05-28 10:02:26 -0300

    Update product_detail_page.dart

--- lib/pages/product_detail_page.dart
+++ lib/pages/product_detail_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
-
 import '../Class/Produto.dart';
 
 class ProductPage extends StatefulWidget {
@@ -16,6 +15,7 @@ class ProductPage extends StatefulWidget {
   }) : super(key: key);
 
   @override
+  // ignore: library_private_types_in_public_api
   _ProductPageState createState() => _ProductPageState();
 }
 
@@ -38,44 +38,45 @@ class _ProductPageState extends State<ProductPage> {
                         imageUrl: widget.produto.imageUrl,
                         fit: BoxFit.cover,
                         placeholder: (context, url) =>
-                            CircularProgressIndicator(),
-                        errorWidget: (context, url, error) => Icon(Icons.error),
+                            const CircularProgressIndicator(),
+                        errorWidget: (context, url, error) =>
+                            const Icon(Icons.error),
                       ),
                     ),
-                    SizedBox(height: 10),
+                    const SizedBox(height: 10),
                     Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 15),
+                      padding: const EdgeInsets.symmetric(horizontal: 15),
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Text(
                             widget.produto.nome_prod,
-                            style: TextStyle(
+                            style: const TextStyle(
                               fontSize: 25,
                               fontWeight: FontWeight.bold,
                             ),
                           ),
-                          SizedBox(height: 2),
+                          const SizedBox(height: 2),
                           Row(
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
                               Text(
                                 widget.produto.categoria,
-                                style: TextStyle(
+                                style: const TextStyle(
                                   fontSize: 18,
                                   color: Color.fromARGB(255, 104, 104, 104),
                                 ),
                               ),
                               Text(
                                 'R\$ ${widget.produto.valor.toStringAsFixed(2)}',
-                                style: TextStyle(
+                                style: const TextStyle(
                                   fontSize: 25,
                                   fontWeight: FontWeight.bold,
                                 ),
                               ),
                             ],
                           ),
-                          SizedBox(height: 2),
+                          const SizedBox(height: 2),
                           Row(
                             mainAxisAlignment: MainAxisAlignment.end,
                             children: [
@@ -85,7 +86,7 @@ class _ProductPageState extends State<ProductPage> {
                                     : 'Indisponível',
                                 style: TextStyle(
                                   color: widget.produto.estoque > 0
-                                      ? Color.fromARGB(255, 15, 181, 32)
+                                      ? const Color.fromARGB(255, 15, 181, 32)
                                       : Colors.red,
                                   fontWeight: FontWeight.bold,
                                   fontSize: 16.0,
@@ -93,14 +94,14 @@ class _ProductPageState extends State<ProductPage> {
                               ),
                             ],
                           ),
-                          SizedBox(height: 20),
+                          const SizedBox(height: 20),
                           Text(
                             widget.produto.desc_prod,
-                            style: TextStyle(
+                            style: const TextStyle(
                               color: Color.fromARGB(255, 103, 103, 103),
                             ),
                           ),
-                          SizedBox(height: 30),
+                          const SizedBox(height: 30),
                           Row(
                             children: [
                               Expanded(
@@ -108,7 +109,11 @@ class _ProductPageState extends State<ProductPage> {
                                   onPressed: widget.produto.estoque > 0
                                       ? () => _adicionarAoCarrinho()
                                       : null,
-                                  child: Padding(
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor:
+                                        const Color.fromARGB(255, 48, 191, 62),
+                                  ),
+                                  child: const Padding(
                                     padding: EdgeInsets.all(15.0),
                                     child: Text(
                                       'Adicionar ao carrinho',
@@ -118,14 +123,11 @@ class _ProductPageState extends State<ProductPage> {
                                       ),
                                     ),
                                   ),
-                                  style: ElevatedButton.styleFrom(
-                                    primary: Color.fromARGB(255, 48, 191, 62),
-                                  ),
                                 ),
                               ),
                             ],
                           ),
-                          SizedBox(height: 20),
+                          const SizedBox(height: 20),
                         ],
                       ),
                     ),
@@ -137,7 +139,7 @@ class _ProductPageState extends State<ProductPage> {
               top: 8,
               left: 8,
               child: IconButton(
-                icon: Icon(Icons.arrow_back),
+                icon: const Icon(Icons.arrow_back),
                 onPressed: () {
                   Navigator.pop(context);
                 },
@@ -167,44 +169,44 @@ class _ProductPageState extends State<ProductPage> {
               mainAxisSize: MainAxisSize.min,
               children: [
                 CachedNetworkImage(
-                  imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnSmQAFKj7G6Nlz8i1wzZ265rArMqQROES6MO-k-riKAtBTqRb',
+                  imageUrl:
+                      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnSmQAFKj7G6Nlz8i1wzZ265rArMqQROES6MO-k-riKAtBTqRb',
                   fit: BoxFit.cover,
-                  placeholder: (context, url) => const CircularProgressIndicator(),
+                  placeholder: (context, url) =>
+                      const CircularProgressIndicator(),
                   errorWidget: (context, url, error) => const Icon(Icons.error),
                 ),
-
-                // Text(
-                //   widget.produto.nome_prod,
-                //   style: const TextStyle(
-                //     fontSize: 20.0,
-                //     fontWeight: FontWeight.bold,
-                //   ),
-                // ),
                 const SizedBox(height: 20.0),
                 RichText(
                   textAlign: TextAlign.center,
                   text: TextSpan(
-                    style: TextStyle(fontSize: 16.0, color: Colors.black),
+                    style: const TextStyle(fontSize: 16.0, color: Colors.black),
                     children: [
-                      TextSpan(
+                      const TextSpan(
                         text: 'O produto ',
                       ),
                       TextSpan(
                         text: widget.produto.nome_prod,
-                        style: TextStyle(fontWeight: FontWeight.bold),
+                        style: const TextStyle(fontWeight: FontWeight.bold),
                       ),
-                      TextSpan(
+                      const TextSpan(
                         text: ' foi adicionado ao carrinho.',
                       ),
                     ],
                   ),
                 ),
-                SizedBox(height: 20.0),
+                const SizedBox(height: 20.0),
                 ElevatedButton(
                   onPressed: () {
                     Navigator.of(context).pop();
                   },
-                  child: Padding(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 48, 191, 62),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                  ),
+                  child: const Padding(
                     padding: EdgeInsets.symmetric(vertical: 10.0),
                     child: Text(
                       'OK',
@@ -212,12 +214,6 @@ class _ProductPageState extends State<ProductPage> {
                         fontSize: 18.0,
                         fontWeight: FontWeight.bold,
                       ),
-                    ),
-                  ),
-                  style: ElevatedButton.styleFrom(
-                    primary: Color.fromARGB(255, 48, 191, 62),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(5.0),
                     ),
                   ),
                 ),
