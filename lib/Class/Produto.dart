05431c21b7b56a06f41fcd0668510eaad9e9ef9c
Gabriel Souza <gabrielsouzase@gmail.com>
2023-05-28 17:57:25 -0300

    Update Produto.dart

--- lib/Class/Produto.dart
+++ lib/Class/Produto.dart
@@ -1,15 +1,13 @@
-// ignore_for_file: file_names
-
 import 'package:uuid/uuid.dart' show Uuid;
 
 class Produto {
-  late String id = const Uuid().v4();
-  final String nome_prod;
-  final double valor;
-  final int estoque;
-  final String imageUrl;
-  final String desc_prod;
-  final String categoria;
+  String id = const Uuid().v4();
+  late final String nome_prod;
+  late final double valor;
+  late final int estoque;
+  late final String imageUrl;
+  late final String desc_prod;
+  late final String categoria;
 
   Produto({
     required this.nome_prod,
@@ -28,6 +26,35 @@ class Produto {
   String get getImageUrl => imageUrl;
   String get getDescProd => desc_prod;
   String get getCategoria => categoria;
+
+  // Setters
+  set setId(String newId) {
+    id = newId;
+  }
+
+  set setNomeProd(String newNomeProd) {
+    nome_prod = newNomeProd;
+  }
+
+  set setValor(double newValor) {
+    valor = newValor;
+  }
+
+  set setEstoque(int newEstoque) {
+    estoque = newEstoque;
+  }
+
+  set setImageUrl(String newImageUrl) {
+    imageUrl = newImageUrl;
+  }
+
+  set setDescProd(String newDescProd) {
+    desc_prod = newDescProd;
+  }
+
+  set setCategoria(String newCategoria) {
+    categoria = newCategoria;
+  }
 
   Map<String, dynamic> toJson() {
     return {
